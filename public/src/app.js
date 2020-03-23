748f7d462fb30d0a07865cffebe7d5824883f9de
Zack <zack.grigor@outlook.com>
2020-03-23 12:41:28 +0000

    Removed Dummy data

--- public/src/app.js
+++ public/src/app.js
@@ -10,17 +10,6 @@ import "./styles/styles.scss";
 
 const store = configureStore();
 
-store.dispatch(addFood({ description: "beef", calories: 250 }));
-store.dispatch(
-  addFood({ description: "Chicken", calories: 150, createdAt: 1000 })
-);
-store.dispatch(addFood({ description: "Lamb", calories: 400 }));
-
-const state = store.getState();
-const visibleCalories = getVisibleCalories(state.calories, state.filters);
-
-console.log(visibleCalories);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
