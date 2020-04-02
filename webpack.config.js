34f5c42fed79fee70aed3ce6c3ed84883eca01e7
Zack <zack.grigor@outlook.com>
2020-04-02 11:49:20 +0100

    setup production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,37 @@
 const path = require("path");
 
-module.exports = {
-  entry: "./public/src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true
-  }
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CSSExtract = new ExtractTextPlugin("styles.css");
+
+module.exports = env => {
+  const isProduction = env === "production";
+  return {
+    entry: "./public/src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: ["css-loader", "sass-loader"]
+          })
+        }
+      ]
+    },
+    plugins: [CSSExtract],
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true
+    }
+  };
 };
 
 //loader
