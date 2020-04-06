73b216ad1532e70c16a7ad1eeab5eadb0b27ddc7
Zack <zack.grigor@outlook.com>
2020-04-06 17:31:06 +0100

    Setup formatted calories and included better formatting on date

--- public/src/components/CaloriesListItem.js
+++ public/src/components/CaloriesListItem.js
@@ -1,21 +1,20 @@
 import React from "react";
-
 import { Link } from "react-router-dom";
+import moment from "moment";
 
 const CaloriesListItem = ({
   dispatch,
   id,
   description,
   calories,
-  createdAt
+  createdAt,
 }) => (
   <div className="caloriesListStyle">
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>
-      {calories} - {createdAt}
-    </p>
+    <p>Amount of calories: {calories} </p>
+    <p> {moment(createdAt).format("MMMM Do, YYYY")}</p>
   </div>
 );
 
