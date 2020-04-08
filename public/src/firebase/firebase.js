0da82f49db1518e5a609ca2d4a5e8bb314ea5c06
Zack <zack.grigor@outlook.com>
2020-04-08 16:39:34 +0100

    continual changes to database

--- public/src/firebase/firebase.js
+++ public/src/firebase/firebase.js
@@ -16,56 +16,51 @@ firebase.analytics();
 
 const database = firebase.database();
 
-database.ref().on("value", (snapshot) => {
-  const val = snapshot.val();
-  console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+//Child remove
+database.ref("calories").on("child_removed", (snapshot) => {
+  console.log(snapshot.key, snapshot.val());
 });
 
+//child Changed
+
+database.ref("calories").on("child_changed", (snapshot) => {
+  console.log(snapshot.key, snapshot.val());
+});
+
+//child added
+database.ref("calories").on("child_added", (snapshot) => {
+  console.log(snapshot.key, snapshot.val());
+});
 // database
-//   .ref("name")
+//   .ref("calories")
 //   .once("value")
 //   .then((snapshot) => {
-//     const val = snapshot.val();
-//     console.log(val);
-//   })
-//   .catch((e) => {
-//     console.log("there was an error");
+//     const calories = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//       calories.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val(),
+//       });
+//     });
+//     console.log(calories);
 //   });
 
-// database
-//   .ref()
-//   .set({
-//     name: "Zack Grigor",
-//     age: 24,
-//     stressLevel: 6,
-//     job: {
-//       title: "Developer",
-//       company: "UoB",
-//     },
-//     location: {
-//       country: "UK",
-//       city: "Guernsey",
-//     },
-//   })
-//   .then(() => {
-//     console.log("data is saved");
-//   })
-//   .catch((e) => {
-//     console.log("This is an error", e);
-//   });
+// database.ref("calories").on("value", (snapshot) => {
+//   const calories = [];
 
-// database.ref().update({
-//   stressLevel: 9,
-//   "job/company": "Amazon",
-//   "location/city": "Brighton",
+//   snapshot.forEach((childSnapshot) => {
+//     calories.push({
+//       id: childSnapshot.key,
+//       ...childSnapshot.val(),
+//     });
+//   });
+//   console.log(calories);
 // });
 
-// database
-//   .ref("isSingle")
-//   .remove()
-//   .then(() => {
-//     console.log("been removed");
-//   })
-//   .catch((e) => {
-//     console.log("there has been an error", e);
-//   });
+// database.ref("calories").push({
+//   Description: "Chicken",
+//   Calories: 100,
+//   note: "lunch Time",
+//   createdAt: "08/04/2020",
+// });
