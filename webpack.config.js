4a0b1f314acfc43e98cb461d9ef53aba0ccfe899
Zack <zack.grigor@outlook.com>
2020-04-11 10:00:10 +0100

    amended test database

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,11 @@ const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CSSExtract = new ExtractTextPlugin("styles.css");
 
-process.env.NODE_ENV = process.env.NODE_ENV || "production";
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 if (process.env.NODE_ENV === "test") {
   require("dotenv").config({ path: ".env.test" });
-} else if (process.env.NODE_ENV === "production") {
+} else if (process.env.NODE_ENV === "development") {
   require("dotenv").config({ path: ".env.development" });
 }
 
