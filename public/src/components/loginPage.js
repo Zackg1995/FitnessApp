9d375be1a7729ca521ae1a6f00de8547f71084da
Zack <zack.grigor@outlook.com>
2020-04-14 16:38:29 +0100

    created login page

--- public/src/components/loginPage.js
+++ public/src/components/loginPage.js
@@ -3,8 +3,14 @@ import { connect } from "react-redux";
 import { startLogin } from "../actions/auth";
 
 export const Login = ({ startLogin }) => (
-  <div>
-    <button onClick={startLogin}>Login</button>
+  <div className="loginLayout">
+    <div className="loginLayout_box">
+      <h1 className="loginLayout_title">FitBud</h1>
+      <p>Here to help!</p>
+      <button onClick={startLogin} className="addLoginButton">
+        Login with Google!
+      </button>
+    </div>
   </div>
 );
 
